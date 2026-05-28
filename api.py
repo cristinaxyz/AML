73fc459cc947adda78a5f9fc7186cda06e8db9cd
cristinaxyz <c.rau.1@student.rug.nl>
2026-05-29 02:32:44 +0300

    Update api.py

--- api.py
+++ api.py
@@ -152,10 +152,5 @@ def main():
     # model = ...
     create_random_file()
 
-    uvicorn.run(app,
-            host = "127.0.0.1",
-            port = 8000,
-            reload = True)
-
 if __name__ == '__main__':
     main()
