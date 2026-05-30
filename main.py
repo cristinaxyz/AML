ed884c6a5eb0d0edbfd5926674b7766ec9e2950e
Patryk Olszowski <patrykolszowski452@gmail.com>
2026-05-30 10:12:05 +0200

    merge for real

--- main.py
+++ main.py
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-import streamlit as st
-import requests
-=======
->>>>>>> 42892a6d (predictions use the real model)
 import io
 
-<<<<<<< HEAD
-def main():
-    """
-    Demo for the users. 
-    """
-    API_URL = "http://127.0.0.1:8000/predict"
-
-    st.set_page_config(page_title = "Brain Tumor Segmentation Demo", layout = "centered")
-    st.markdown("<h1 style='color: pink'>Brain Tumor Segmentation Demo<h1>", unsafe_allow_html=True)
-    st.markdown("<h3 style='color:pink'>Upload a '.h5' MRI scan slice. Returns a '.png' file with the segmentation mask with 3 regions of the tumor identified. Our model performs segmentation on MRI scan slices. We trained (model info). It was trained on BraTS2020 dataset, which included MRI scan slices with segmentations performed manually by neuro-radiologists. </h3>", unsafe_allow_html=True)
-    uploading = st.file_uploader("Upload MRI scan '.h5'...", type=["h5"])
-    if uploading != None:
-        st.success("File uploaded! :D")
-        if st.button("Run Segmentation"):
-            files = {"scan": (uploading.name, uploading.getvalue())}
-            response = requests.post(API_URL, files=files)
-            if response.status_code == 200:
-                st.success("Prediction is done!")
-                image = Image.open(io.BytesIO(response.content))
-                st.image(image, caption = "Segmentation Mask", width='stretch')
-                st.download_button(label = "Download '.png'!", data=response.content, file_name="segmentation.png")
-            else:
-                st.error(f"Sorry, prediction failed: {response.status_code}. :(")
-=======
 import h5py
 import numpy as np
 import torch
@@ -166,7 +137,6 @@ def main():
     model = UNet(3, MODEL_BATCH_NORM)
     model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
     model.eval()
->>>>>>> 42892a6d (predictions use the real model)
 
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
